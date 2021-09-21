1350894e402ded925f16311e1cfabc58e9223ea2
Simon <s173255@student.dtu.dk>
2021-09-21 10:27:59 +0200

    Cards Component

--- src/Components/grid.js
+++ src/Components/grid.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import Jumbotron from './jumbotron';
+import Cards from './cards'
 
 const GridLayout = () => {
     return (
@@ -10,57 +11,8 @@ const GridLayout = () => {
                 <Row className="justify-content-md-center">
                     <Jumbotron />
                 </Row>
-                <Row style={{ padding: "2rem" }}><h2>Spilles i dag</h2></Row>
-                <Row style={{ padding: "2rem" }} className="justify-content-md-center">
-                    
-                    <CardGroup>
-                        <Card border="dark" style={{ width: '15rem'}}>
-                            <Card.Body>
-                                <Card.Title>Suicide Squad</Card.Title>
-                                <Card.Link href="#">18:00</Card.Link>
-                                <Card.Link href="#">21:00</Card.Link>
-                                <Card.Link href="#">22:30</Card.Link>
-                            </Card.Body>
-                        </Card>
 
-                        <Card border="dark" style={{ width: '15rem'}}>
-                            <Card.Body>
-                                <Card.Title>Avengers</Card.Title>
-                                <Card.Link href="#">18:00</Card.Link>
-                                <Card.Link href="#">21:00</Card.Link>
-                            </Card.Body>
-                        </Card>
-
-                        <Card border="dark" style={{ width: '15rem'}}>
-                            <Card.Body>
-                                <Card.Title>Jaws</Card.Title>
-                                <Card.Link href="#">19:00</Card.Link>
-                                <Card.Link href="#">21:30</Card.Link>
-                            </Card.Body>
-                        </Card>
-
-                        <Card border="dark" style={{ width: '15rem'}}>
-                            <Card.Body>
-                                <Card.Title>Interstellar</Card.Title>
-                                <Card.Link href="#">18:00</Card.Link>
-                            </Card.Body>
-                        </Card>
-
-                        <Card border="dark" style={{ width: '15rem'}}>
-                            <Card.Body>
-                                <Card.Title>Interstellar</Card.Title>
-                                <Card.Link href="#">00:00</Card.Link>
-                            </Card.Body>
-                        </Card>
-
-                        <Card border="dark" style={{ width: '15rem'}}>
-                            <Card.Body>
-                                <Card.Title>Interstellar</Card.Title>
-                                <Card.Link href="#">21:00</Card.Link>
-                            </Card.Body>
-                        </Card>
-                    </CardGroup>
-                </Row>
+                <Cards />
 
             </Container>
         </>
