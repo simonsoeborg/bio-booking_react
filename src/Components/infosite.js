9359b0048f7e66caea364c715c4eda69b23c1d93
Magnus <s195401@student.dtu.dk>
2021-09-26 14:02:57 +0200

    Very Basic layout
    Meget basic layout med navn, email og antal sæder.
    Bug: dobbelt view af infosite

--- src/Components/infosite.js
+++ src/Components/infosite.js
@@ -1,9 +1,51 @@
-import { Container } from "react-bootstrap"
+import {Container, Row, Form, Button, Col, Card} from "react-bootstrap"
+
 
 
 const InfoSite = () =>{
     return(
-        <Container></Container>
+        <Container>
+            <Row xs={3} md={2}>
+            <Form>
+
+                <Form.Group >
+                    <Form.Label>Navn</Form.Label>
+                    <Form.Control type="fullname" placeholder="Indtast dit fulde navn" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                
+                <Form.Group>
+                    <Form.Label>Sæder</Form.Label>
+                    <Form.Select>
+                        <option>Vælg antal sæder</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </Form.Select>
+                </Form.Group>
+                <Button variant="outline-primary" type="submit">
+                    Place Booking
+                </Button>
+            </Form>
+            </Row>
+            <Row xs={2} md={3}>
+                <Col md={{span: 3, offset: 10}}>
+                    <Card border="success" style={{width: '10rem'}}> 
+                        <Card.Body>
+                            <Card.Text>Række 1, Sæde 3</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+        
 
     )
 
