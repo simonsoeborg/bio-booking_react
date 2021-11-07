4077b44ee7e8c26e11c3659828cc0a28d2a6311a
Simon <s173255@student.dtu.dk>
2021-11-07 23:11:30 +0100

    Movie CRUD ( I think )

---
+++ src/Assets/DTO/getmovies.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+function getMovies() {
+  axios.get(``).then((data) => {
+    const movie = data.data;
+    this.movies.setState({ movie });
+  });
+
+  return this.state.movies;
+}
+
+function getMovie(id) {
+  axios.get(``).then((data) => {
+    const movie = data.data;
+    this.movie.setState({ movie });
+  });
+
+  return this.state.movie;
+}
+
+function putMovie(id, data) {
+  const res = await axios.put(` WHERE Something = ${id}`, data);
+  this.setState.movie({ updatedAt: res.data.updatedAt });
+}
+
+function createMovie(data) {
+  const res = await axios.create(``, data);
+  this.setState.movie({ updatedAt: res.data.updatedAt });
+}
