592ab3dabdff1cc0ea146373911542e7ea8c2499
Simon <s173255@student.dtu.dk>
2021-11-11 10:01:29 +0100

    Nope

--- src/Assets/Services/MovieService.js
+++ src/Assets/Services/MovieService.js
@@ -1,51 +1,28 @@
-const movieApiUrl = "https://localhost:44349/api/Movie";
+import api from "../api";
+const movieApiUrl = "https://localhost:44349/api/Movie/";
 
-class MovieService {
-  get = async (params) => {
-    const options = {
-      method: "GET",
-    };
-    const request = new Request(movieApiUrl + "/" + params, options);
-    const response = await fetch(request);
-    return response.json();
-  };
+const getMovies = () => {
+  return api.get(movieApiUrl);
+};
 
-  post = async (model) => {
-    const headers = new Headers();
-    headers.append.toString("Content-Type", "applications/json");
-    var options = {
-      method: "Post",
-      headers,
-      body: JSON.stringify(model),
-    };
-    const request = new Request(movieApiUrl, options);
-    const response = await fetch(request);
-    return response;
-  };
+const getMovieById = (id) => {
+  return api.get(movieApiUrl + id);
+};
 
-  put = async (model) => {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    var options = {
-      method: "PUT",
-      headers,
-      body: JSON.stringify(model),
-    };
-    const request = new Request(movieApiUrl, options);
-    const response = await fetch(request);
-    return response;
-  };
-  delete = async (id) => {
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    const options = {
-      method: "DELETE",
-      headers,
-    };
-    const request = new Request(movieApiUrl + "/" + id, options);
-    const response = await fetch(request);
-    return response;
-  };
-}
+const addMovie = (data) => {
+  return api.post(movieApiUrl, data);
+};
+const updateMovie = (id, data) => {
+  return api.put(movieApiUrl + id, data);
+};
+const deleteMovie = (id) => {
+  return api.delete(movieApiUrl + id);
+};
 
-export default MovieService;
+export default {
+  getMovies,
+  getMovieById,
+  addMovie,
+  updateMovie,
+  deleteMovie,
+};
--- src/Components/moviepage.js
+++ src/Components/moviepage.js
@@ -1,185 +1,145 @@
-import * as React from "react";
-import { observer } from "mobx";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import defaultImg from "../images/default-movie.jpg";
 import { Image, Col, Row, Container } from "react-bootstrap";
-import MovieStore from "../Assets/Stores/MovieStore";
+import MovieService from "../Assets/Services/MovieService";
 
-class MovieView extends React.Component {
-  componentDidMount() {
-    this.props.MovieStore.getMoviesAsync();
+const MovieView = () => {
+  const initState = {
+    movie: null,
+  };
+
+  const [myId, setMyId] = useState(0);
+  const [movie, setMovie] = useState(initState);
+
+  let { id } = useParams();
+  console.log(id);
+  setMyId({ id: id });
+
+  const GetMovie = () => {
+    console.log(`id: ${myId}`);
+    MovieService.getMovieById({ myId })
+      .then((response) => {
+        setMovie(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // useEffect(() => {
+  //   GetMovie();
+  // }, []);
+  while (movie.movie === null) {
+    GetMovie();
   }
 
-  render() {
-    const id = this.props.match.params.id;
-    const movie = MovieStore.getState.map((item) => {
-      if (item.id === id) {
-        return item;
-      } else {
-        return null;
-      }
-    });
-    if (movie != null) {
-      return (
-        <Container fluid>
-          <Row>
-            <Col xs={6} md={4}>
-              <Image id="default-img" src={movie.posterImageString} rounded />
-            </Col>
-            <Col xs={6} md={6}>
-              <Row>
-                <h1>{movie.movieName}</h1>
-              </Row>
-              <Row>
-                <p>
-                  Movie Description
-                  <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke
-                  bare tilfældig tekst. Det stammer fra et stykke litteratur på
-                  latin fra år 45 f.kr., hvilket gør teksten over 2000 år
-                  gammel. Richard McClintock, professor i latin fra
-                  Hampden-Sydney universitet i Virginia, undersøgte et af de
-                  mindst kendte ord "consectetur" fra en del af Lorem Ipsum, og
-                  fandt frem til dets oprindelse ved at studere brugen gennem
-                  klassisk litteratur. Lorem Ipsum stammer fra afsnittene
-                  1.10.32 og 1.10.33 fra "de Finibus Bonorum et Malorum" (Det
-                  gode og ondes ekstremer), som er skrevet af Cicero i år 45
-                  f.kr. Bogen, som var meget populær i renæssancen, er en
-                  afhandling om etik. Den første linie af Lorem Ipsum "Lorem
-                  ipsum dolor sit amet..." kommer fra en linje i afsnit 1.10.32.
-                </p>
-              </Row>
-              <Row>
-                <Col>
-                  <h4>IMDB Rating</h4>
-                </Col>
-                <Col>
-                  <h4>Movie Director, Movie Actors</h4>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={12}>
-              <h2>{movie.movieFeatureDates}</h2>
-            </Col>
-          </Row>
-        </Container>
-      );
-    }
+  if (movie.movie === null || movie.movie === undefined) {
+    return <DefaultMoviePage />;
+  } else {
+    return <LoadedMoviePage movie={movie} />;
+  }
+};
+
+function LoadedMoviePage(movie) {
+  console.log(movie);
+
+  if (!movie) return <h1>Loading....</h1>;
+  else {
+    return (
+      <Container fluid>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image id="default-img" src={movie.posterImageString} rounded />
+          </Col>
+          <Col xs={6} md={6}>
+            <Row>
+              <h1>{movie.movieName}</h1>
+            </Row>
+            <Row>
+              <p>
+                Movie Description
+                <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke bare
+                tilfældig tekst. Det stammer fra et stykke litteratur på latin
+                fra år 45 f.kr., hvilket gør teksten over 2000 år gammel.
+                Richard McClintock, professor i latin fra Hampden-Sydney
+                universitet i Virginia, undersøgte et af de mindst kendte ord
+                "consectetur" fra en del af Lorem Ipsum, og fandt frem til dets
+                oprindelse ved at studere brugen gennem klassisk litteratur.
+                Lorem Ipsum stammer fra afsnittene 1.10.32 og 1.10.33 fra "de
+                Finibus Bonorum et Malorum" (Det gode og ondes ekstremer), som
+                er skrevet af Cicero i år 45 f.kr. Bogen, som var meget populær
+                i renæssancen, er en afhandling om etik. Den første linie af
+                Lorem Ipsum "Lorem ipsum dolor sit amet..." kommer fra en linje
+                i afsnit 1.10.32.
+              </p>
+            </Row>
+            <Row>
+              <Col>
+                <h4>IMDB Rating</h4>
+              </Col>
+              <Col>
+                <h4>Movie Director, Movie Actors</h4>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={12}>
+            <h2>{movie.movieFeatureDates}</h2>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
-// import defaultImg from "../images/default-movie.jpg";
-// import { Container, Row, Col, Image } from "react-bootstrap";
-// import { useParams } from "react-router-dom";
-// // https://howtojsonapi.com/react.html
-
-// export default function MoviePage() {
-//   let { id } = useParams();
-//   console.log(`Id: ${id}`);
-//   if (id === undefined) {
-//     return <DefaultMoviePage />;
-//   } else {
-//     return <LoadedMoviePage id={id} />;
-//   }
-// }
-
-// function LoadedMoviePage(movie) {
-//   console.log(movie);
-
-//   if (!movie) return <h1>Loading....</h1>;
-//   else {
-//     return (
-//       <Container fluid>
-//         <Row>
-//           <Col xs={6} md={4}>
-//             <Image id="default-img" src={movie.posterImageString} rounded />
-//           </Col>
-//           <Col xs={6} md={6}>
-//             <Row>
-//               <h1>{movie.movieName}</h1>
-//             </Row>
-//             <Row>
-//               <p>
-//                 Movie Description
-//                 <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke bare
-//                 tilfældig tekst. Det stammer fra et stykke litteratur på latin
-//                 fra år 45 f.kr., hvilket gør teksten over 2000 år gammel.
-//                 Richard McClintock, professor i latin fra Hampden-Sydney
-//                 universitet i Virginia, undersøgte et af de mindst kendte ord
-//                 "consectetur" fra en del af Lorem Ipsum, og fandt frem til dets
-//                 oprindelse ved at studere brugen gennem klassisk litteratur.
-//                 Lorem Ipsum stammer fra afsnittene 1.10.32 og 1.10.33 fra "de
-//                 Finibus Bonorum et Malorum" (Det gode og ondes ekstremer), som
-//                 er skrevet af Cicero i år 45 f.kr. Bogen, som var meget populær
-//                 i renæssancen, er en afhandling om etik. Den første linie af
-//                 Lorem Ipsum "Lorem ipsum dolor sit amet..." kommer fra en linje
-//                 i afsnit 1.10.32.
-//               </p>
-//             </Row>
-//             <Row>
-//               <Col>
-//                 <h4>IMDB Rating</h4>
-//               </Col>
-//               <Col>
-//                 <h4>Movie Director, Movie Actors</h4>
-//               </Col>
-//             </Row>
-//           </Col>
-//         </Row>
-//         <Row>
-//           <Col xs={12} md={12}>
-//             <h2>{movie.movieFeatureDates}</h2>
-//           </Col>
-//         </Row>
-//       </Container>
-//     );
-//   }
-// }
-
-// function DefaultMoviePage() {
-//   return (
-//     <Container fluid>
-//       <Row>
-//         <Col xs={6} md={4}>
-//           <Image id="default-img" src={defaultImg} rounded />
-//         </Col>
-//         <Col xs={6} md={6}>
-//           <Row>
-//             <h1>Movie Title</h1>
-//           </Row>
-//           <Row>
-//             <p>
-//               Movie Description
-//               <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke bare
-//               tilfældig tekst. Det stammer fra et stykke litteratur på latin fra
-//               år 45 f.kr., hvilket gør teksten over 2000 år gammel. Richard
-//               McClintock, professor i latin fra Hampden-Sydney universitet i
-//               Virginia, undersøgte et af de mindst kendte ord "consectetur" fra
-//               en del af Lorem Ipsum, og fandt frem til dets oprindelse ved at
-//               studere brugen gennem klassisk litteratur. Lorem Ipsum stammer fra
-//               afsnittene 1.10.32 og 1.10.33 fra "de Finibus Bonorum et Malorum"
-//               (Det gode og ondes ekstremer), som er skrevet af Cicero i år 45
-//               f.kr. Bogen, som var meget populær i renæssancen, er en afhandling
-//               om etik. Den første linie af Lorem Ipsum "Lorem ipsum dolor sit
-//               amet..." kommer fra en linje i afsnit 1.10.32.
-//             </p>
-//           </Row>
-//           <Row>
-//             <Col>
-//               <h4>IMDB Rating</h4>
-//             </Col>
-//             <Col>
-//               <h4>Movie Director, Movie Actors</h4>
-//             </Col>
-//           </Row>
-//         </Col>
-//       </Row>
-//       <Row>
-//         <Col xs={12} md={12}>
-//           <h2>Movie Play Times</h2>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// }
+function DefaultMoviePage() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={6} md={4}>
+          <Image id="default-img" src={defaultImg} rounded />
+        </Col>
+        <Col xs={6} md={6}>
+          <Row>
+            <h1>Movie Title</h1>
+          </Row>
+          <Row>
+            <p>
+              Movie Description
+              <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke bare
+              tilfældig tekst. Det stammer fra et stykke litteratur på latin fra
+              år 45 f.kr., hvilket gør teksten over 2000 år gammel. Richard
+              McClintock, professor i latin fra Hampden-Sydney universitet i
+              Virginia, undersøgte et af de mindst kendte ord "consectetur" fra
+              en del af Lorem Ipsum, og fandt frem til dets oprindelse ved at
+              studere brugen gennem klassisk litteratur. Lorem Ipsum stammer fra
+              afsnittene 1.10.32 og 1.10.33 fra "de Finibus Bonorum et Malorum"
+              (Det gode og ondes ekstremer), som er skrevet af Cicero i år 45
+              f.kr. Bogen, som var meget populær i renæssancen, er en afhandling
+              om etik. Den første linie af Lorem Ipsum "Lorem ipsum dolor sit
+              amet..." kommer fra en linje i afsnit 1.10.32.
+            </p>
+          </Row>
+          <Row>
+            <Col>
+              <h4>IMDB Rating</h4>
+            </Col>
+            <Col>
+              <h4>Movie Director, Movie Actors</h4>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={12}>
+          <h2>Movie Play Times</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default MovieView;
