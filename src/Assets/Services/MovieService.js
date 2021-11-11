5e0647d2ed0f8916359f1e9484743f334764866c
Simon <s173255@student.dtu.dk>
2021-11-11 10:51:54 +0100

    MoviePage virker nu med CRUD

--- src/Assets/Services/MovieService.js
+++ src/Assets/Services/MovieService.js
@@ -1,28 +1,46 @@
+import { Component } from "react";
 import api from "../api";
+import movieStore from "../Stores/MovieStore";
+
 const movieApiUrl = "https://localhost:44349/api/Movie/";
 
-const getMovies = () => {
-  return api.get(movieApiUrl);
+const initialState = {
+  movies: [],
+  movie: JSON,
 };
 
-const getMovieById = (id) => {
-  return api.get(movieApiUrl + id);
-};
+class MovieService extends Component {
+  state = initialState;
 
-const addMovie = (data) => {
-  return api.post(movieApiUrl, data);
-};
-const updateMovie = (id, data) => {
-  return api.put(movieApiUrl + id, data);
-};
-const deleteMovie = (id) => {
-  return api.delete(movieApiUrl + id);
-};
+  getMovies = async () => {
+    await api.get(movieApiUrl).then((response) => {
+      movieStore.setState({ movies: response.data });
+    });
+  };
 
-export default {
-  getMovies,
-  getMovieById,
-  addMovie,
-  updateMovie,
-  deleteMovie,
-};
+  getMovieById = async (id) => {
+    await api.get(movieApiUrl + id).then((response) => {
+      movieStore.setState({ movie: response.data });
+    });
+  };
+
+  addMovie = async (data) => {
+    await api.post(movieApiUrl, JSON.stringify(data)).then((response) => {
+      console.log(response);
+    });
+  };
+
+  updateMovie = async (id, data) => {
+    await api.put(movieApiUrl + id, JSON.stringify(data)).then((response) => {
+      console.log(response);
+    });
+  };
+
+  deleteMovie = async (id) => {
+    await api.delete(movieApiUrl + id).then((response) => {
+      console.log(response);
+    });
+  };
+}
+
+export default MovieService;
--- src/Components/moviepage.js
+++ src/Components/moviepage.js
@@ -1,78 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import defaultImg from "../images/default-movie.jpg";
+import React, { Component } from "react";
 import { Image, Col, Row, Container } from "react-bootstrap";
 import MovieService from "../Assets/Services/MovieService";
+import api from "../Assets/api";
 
-const MovieView = () => {
-  const initState = {
-    movie: null,
-  };
+const movieApiUrl = "https://localhost:44349/api/Movie/";
 
-  const [myId, setMyId] = useState(0);
-  const [movie, setMovie] = useState(initState);
-
-  let { id } = useParams();
-  console.log(id);
-  setMyId({ id: id });
-
-  const GetMovie = () => {
-    console.log(`id: ${myId}`);
-    MovieService.getMovieById({ myId })
-      .then((response) => {
-        setMovie(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  // useEffect(() => {
-  //   GetMovie();
-  // }, []);
-  while (movie.movie === null) {
-    GetMovie();
-  }
-
-  if (movie.movie === null || movie.movie === undefined) {
-    return <DefaultMoviePage />;
-  } else {
-    return <LoadedMoviePage movie={movie} />;
-  }
+const initialState = {
+  movies: [],
+  movie: null,
 };
 
-function LoadedMoviePage(movie) {
-  console.log(movie);
+export default class MoviePage extends Component {
+  state = initialState;
 
+  async componentDidMount() {
+    let id = this.props.match.params.id;
+    await this.setState({ movies: MovieService.getMovies });
+    await api.get(movieApiUrl + id).then((response) => {
+      this.setState({ movie: response.data });
+    });
+    await this.setState({ movie: this.state.movie });
+  }
+
+  render() {
+    if (this.state.movie === null) {
+      return <h1>Loading...</h1>;
+    } else {
+      return <LoadedMoviePage movie={this.state.movie} />;
+    }
+  }
+}
+
+function LoadedMoviePage(movie) {
   if (!movie) return <h1>Loading....</h1>;
   else {
     return (
       <Container fluid>
         <Row>
           <Col xs={6} md={4}>
-            <Image id="default-img" src={movie.posterImageString} rounded />
+            <Image
+              id="default-img"
+              src={`${movie.movie.posterImageString}`}
+              rounded
+            />
           </Col>
           <Col xs={6} md={6}>
             <Row>
-              <h1>{movie.movieName}</h1>
+              <h1>{movie.movie.movieName}</h1>
             </Row>
             <Row>
               <p>
-                Movie Description
-                <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke bare
-                tilfældig tekst. Det stammer fra et stykke litteratur på latin
-                fra år 45 f.kr., hvilket gør teksten over 2000 år gammel.
-                Richard McClintock, professor i latin fra Hampden-Sydney
-                universitet i Virginia, undersøgte et af de mindst kendte ord
-                "consectetur" fra en del af Lorem Ipsum, og fandt frem til dets
-                oprindelse ved at studere brugen gennem klassisk litteratur.
-                Lorem Ipsum stammer fra afsnittene 1.10.32 og 1.10.33 fra "de
-                Finibus Bonorum et Malorum" (Det gode og ondes ekstremer), som
-                er skrevet af Cicero i år 45 f.kr. Bogen, som var meget populær
-                i renæssancen, er en afhandling om etik. Den første linie af
-                Lorem Ipsum "Lorem ipsum dolor sit amet..." kommer fra en linje
-                i afsnit 1.10.32.
+                Summary
+                <br />
+                Skal hentes fra IMDB
               </p>
             </Row>
             <Row>
@@ -86,60 +66,16 @@ function LoadedMoviePage(movie) {
           </Col>
         </Row>
         <Row>
-          <Col xs={12} md={12}>
-            <h2>{movie.movieFeatureDates}</h2>
+          <Col xs={6} md={6}>
+            <Col>
+              <h4>Visningsdage: </h4>
+            </Col>
+            <Col>
+              <p>{movie.movie.movieFeatureDates}</p>
+            </Col>
           </Col>
         </Row>
       </Container>
     );
   }
 }
-
-function DefaultMoviePage() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col xs={6} md={4}>
-          <Image id="default-img" src={defaultImg} rounded />
-        </Col>
-        <Col xs={6} md={6}>
-          <Row>
-            <h1>Movie Title</h1>
-          </Row>
-          <Row>
-            <p>
-              Movie Description
-              <br />I modsætning til hvad mange tror, er Lorem Ipsum ikke bare
-              tilfældig tekst. Det stammer fra et stykke litteratur på latin fra
-              år 45 f.kr., hvilket gør teksten over 2000 år gammel. Richard
-              McClintock, professor i latin fra Hampden-Sydney universitet i
-              Virginia, undersøgte et af de mindst kendte ord "consectetur" fra
-              en del af Lorem Ipsum, og fandt frem til dets oprindelse ved at
-              studere brugen gennem klassisk litteratur. Lorem Ipsum stammer fra
-              afsnittene 1.10.32 og 1.10.33 fra "de Finibus Bonorum et Malorum"
-              (Det gode og ondes ekstremer), som er skrevet af Cicero i år 45
-              f.kr. Bogen, som var meget populær i renæssancen, er en afhandling
-              om etik. Den første linie af Lorem Ipsum "Lorem ipsum dolor sit
-              amet..." kommer fra en linje i afsnit 1.10.32.
-            </p>
-          </Row>
-          <Row>
-            <Col>
-              <h4>IMDB Rating</h4>
-            </Col>
-            <Col>
-              <h4>Movie Director, Movie Actors</h4>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} md={12}>
-          <h2>Movie Play Times</h2>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
-
-export default MovieView;
