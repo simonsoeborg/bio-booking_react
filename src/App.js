0f1ec6222d67e1c3e6d5161f0344eb98e9a49d42
Kasper Baun <kasperbaun@hotmail.com>
2021-11-27 19:05:17 +0100

    removed react-seat-picker dependency and fixed a few imports that had uppercase imports on lowercase names

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./App.css";
 import Init from "./Components/Home";
 import Booking from "./Components/Booking";
-import Program from "./Components/Program";
-import Movie from "./Components/Movie";
-import LoginPage from "./Components/LoginPage";
+import Program from "./Components/program";
+import Movie from "./Components/movie";
+import LoginPage from "./Components/loginpage";
 import AdminPage from "./Components/Admin";
 import EditMovie from "./Components/AdminComponents/PartialComponents/EditMovie";
 import CreateMovie from "./Components/AdminComponents/PartialComponents/CreateMovie";
-import EditUser from "./Components/AdminComponents/PartialComponents/EditUser";
+import EditUser from "./Components/AdminComponents/PartialComponents/EditMovie";
 import CreateTheater from "./Components/AdminComponents/PartialComponents/CreateTheater";
 import EditTheater from "./Components/AdminComponents/PartialComponents/EditTheater";
 import Upcoming from './Components/Upcoming';
 
 import { HashRouter, Route, Switch } from "react-router-dom";
-import BioNavbar from "./Components/Navbar";
+import BioNavbar from "./Components/navbar";
 
 const App = () => {
   return (
@@ -24,7 +24,7 @@ const App = () => {
         <Switch>
           <Route path="/booking/:id" component={Booking} />
           <Route path="/booking/" component={Booking} />
-          <Route path="/upcoming" component={Upcoming}/>
+          <Route path="/upcoming" component={Upcoming} />
           <Route path="/program" component={Program} />
           <Route path="/movie/:id" component={Movie} />
           <Route exact path="/" component={Init} />
